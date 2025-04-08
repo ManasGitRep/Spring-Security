4a9d4590310a0b7193630d2f89c23bd70826b30b
Manas Choudhary <mannchoudhary3431@gmail.com>
2025-04-08 11:51:49 +0530

    Resolved merge conflict in User.java

--- src/main/java/com/springsecurity/model/User.java
+++ src/main/java/com/springsecurity/model/User.java
@@ -1,7 +1,5 @@
 package com.springsecurity.model;
 
-
-<<<<<<< HEAD
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -9,26 +7,30 @@ import jakarta.persistence.Id;
 
 @Entity
 public class User {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-=======
-public class User {
->>>>>>> 20a48395b4715dd73e7ee7e61a09e43c08f6120e
+
     private String username;
     private String password;
     private String email;
 
-<<<<<<< HEAD
+    public User() {
+    }
+
+    public User(String username, String password, String email) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
     public Long getId() {
         return id;
     }
 
     public void setId(Long id) {
         this.id = id;
-=======
-    public User(String manas, String number, String mail) {
->>>>>>> 20a48395b4715dd73e7ee7e61a09e43c08f6120e
     }
 
     public String getUsername() {
